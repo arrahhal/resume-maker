a9e21ee49b6697a6184511044ebcf4541fd1a71c
arrahhal <arrahhal@proton.me>
2024-03-21 12:10:08 +0300

    Add some icons components

---
+++ src/components/Icons.jsx
@@ -0,0 +1,53 @@
+export function UploadIcon({ width = "24px", height = "24px", fill = "#000000" }) {
+  return (
+    <svg width={width} height={height} viewBox="0 0 20 20" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" fill={fill}>
+      <g strokeWidth="0"></g>
+      <g strokeLinecap="round" strokeLinejoin="round"></g>
+      <g>
+        <g id="Page-1" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
+          <g id="Dribbble-Light-Preview" transform="translate(-260.000000, -6959.000000)" fill={fill}>
+            <g id="icons" transform="translate(56.000000, 160.000000)">
+              <path d="M222,6816 C222,6816.552 221.552,6817 221,6817 L207,6817 C206.448,6817 206,6816.552 206,6816 L206,6814 C206,6813.448 206.448,6813 207,6813 L221,6813 C221.552,6813 222,6813.448 222,6814 L222,6816 Z M204,6813 L204,6817 C204,6818.105 204.895,6819 206,6819 L222,6819 C223.105,6819 224,6818.105 224,6817 L224,6813 C224,6811.895 223.105,6811 222,6811 L215,6811 L215,6803.207 C215,6802.761 215.539,6802.538 215.853,6802.853 L217.536,6804.536 C217.926,6804.927 218.56,6804.927 218.95,6804.536 C219.34,6804.146 219.34,6803.513 218.95,6803.122 L215.413,6799.585 C214.632,6798.805 213.367,6798.805 212.587,6799.585 C212.409,6799.763 212.846,6799.326 209.05,6803.122 C208.66,6803.512 208.66,6804.146 209.05,6804.536 C209.441,6804.927 210.074,6804.927 210.465,6804.536 L212.146,6802.855 C212.461,6802.54 213,6802.763 213,6803.209 L213,6811 L206,6811 C204.895,6811 204,6811.895 204,6813 L204,6813 Z" id="arrow_up-[#267]"> </path>
+            </g>
+          </g>
+        </g>
+      </g>
+    </svg>
+  )
+}
+
+export function DeleteIcon({ width = "24px", height = "24px", fill = "#000000" }) {
+  return (
+    <svg width={width} height={height} viewBox="0 -0.5 21 21" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" fill={fill}>
+      <g strokeWidth="0"></g>
+      <g strokeLinecap="round" strokeLinejoin="round"></g>
+      <g>
+        <g id="Page-1" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
+          <g id="Dribbble-Light-Preview" transform="translate(-179.000000, -360.000000)" fill={fill}>
+            <g id="icons" transform="translate(56.000000, 160.000000)">
+              <path d="M130.35,216 L132.45,216 L132.45,208 L130.35,208 L130.35,216 Z M134.55,216 L136.65,216 L136.65,208 L134.55,208 L134.55,216 Z M128.25,218 L138.75,218 L138.75,206 L128.25,206 L128.25,218 Z M130.35,204 L136.65,204 L136.65,202 L130.35,202 L130.35,204 Z M138.75,204 L138.75,200 L128.25,200 L128.25,204 L123,204 L123,206 L126.15,206 L126.15,220 L140.85,220 L140.85,206 L144,206 L144,204 L138.75,204 Z" id="delete-[#1487]"> </path>
+            </g>
+          </g>
+        </g>
+      </g>
+    </svg>
+  )
+}
+
+export function CloseIcon({ width = "24px", height = "24px", fill = "#000000" }) {
+  return (
+    <svg width={width} height={height} viewBox="0 -0.5 21 21" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" fill={fill}>
+      <g strokeWidth="0"></g>
+      <g strokeLinecap="round" strokeLinejoin="round"></g>
+      <g>
+        <g id="Page-1" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
+          <g id="Dribbble-Light-Preview" transform="translate(-419.000000, -240.000000)" fill={fill}>
+            <g id="icons" transform="translate(56.000000, 160.000000)">
+              <polygon id="close-[#1511]" points="375.0183 90 384 98.554 382.48065 100 373.5 91.446 364.5183 100 363 98.554 371.98065 90 363 81.446 364.5183 80 373.5 88.554 382.48065 80 384 81.446"> </polygon>
+            </g>
+          </g>
+        </g>
+      </g>
+    </svg>
+  )
+}
