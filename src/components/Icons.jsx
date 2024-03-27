b6c86dbd7f7960a1a07b780150a521fcd64b1cb7
arrahhal <arrahhal@proton.me>
2024-03-28 01:37:55 +0300

    more icons

--- src/components/Icons.jsx
+++ src/components/Icons.jsx
@@ -99,3 +99,24 @@ export function AddressIcon({ width = "12px", height = "12px" }) {
     </svg>
   )
 }
+
+export function PlusIcon({ width = "12px", height = "12px" }) {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width={width} height={height} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+      <path d="M12 5l0 14" />
+      <path d="M5 12l14 0" />
+    </svg>
+  )
+}
+
+export function EditIcon({ width = "12px", height = "12px" }) {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width={width} height={height} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+      <path d="M7 7h-1a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-1" />
+      <path d="M20.385 6.585a2.1 2.1 0 0 0 -2.97 -2.97l-8.415 8.385v3h3l8.385 -8.415z" />
+      <path d="M16 5l3 3" />
+    </svg>
+  )
+}
